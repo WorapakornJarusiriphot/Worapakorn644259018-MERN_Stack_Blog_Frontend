d4ec523053d5cfc7b2ae57cc61a022af0b61cc0b
worapakorn <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2023-12-14 23:47:10 +0700

    Add .env And Update Header.jsx , LoginPage.jsx , RegisterPage.jsx
    Add .env And Update Header.jsx , LoginPage.jsx , RegisterPage.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
 import { UserContext } from '../context/UserContext'
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const Header = () => {
+    const { setUserInfo, userInfo } = useContext(UserContext);
+    const username = userInfo?.username;
+    const logout = () => {
+        fetch(`${baseURL}/logout`, {
+            credentials: "include",
+            method: "POST",
+        })
+        setUserInfo(null);
+    }
     return (
         <header>
             <Link to={"/"} className='logo'>
                 SE NPRU blog
             </Link>
             <nav>
-                <Link to={"/login"}>Login</Link>
-                <Link to={"/register"}>Register</Link>
+                {username && (
+                    <>
+                        <Link to={"/create"}>Create new post</Link>
+                        <a onClick={logout}>Logout ({username})</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to={"/login"}>Login</Link>
+                        <Link to={"/register"}>Register</Link>
+                    </>
+                )}
             </nav>
         </header>
     )
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default RegisterPage
+  const register = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${baseURL}/register`, {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("Registration successful !");
+    } else {
+      alert("Registration failed !");
+    }
+  };
+
+  return (
+    <form className='register' onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        name='username'
+        placeholder='username'
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        name='password'
+        placeholder='password'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default RegisterPage;
